fa88b0b7576d8db59ffa00c504fd46a75c1e4c50
alu0101327679 <alu0101327679@ull.edu.es>
2023-03-16 11:08:53 +0000

    autocomentario

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,123 @@
 /**
- * funcion que suma __desde__ 2 numeros y devuelve el resultado
- * @param firstNumber  primer numero a operar
- * @param secondNumber segundo numero a operar
- * @param remainingNumbers resto de numeros a opear
- * @returns resultado de la suma de todos los numeros
+ *  CLASE para tratar con datos de tipo imperial
+ * la cual almacena sus datos en pies
  */
-export function add(firstNumber: number, secondNumber: number) {
-  return firstNumber + secondNumber;
+export class ImperialLength {
+  /**
+   * constructor de la clase
+   * @param numero numero en pies
+   */
+  constructor(private numero: number = 0) {}
+  /**
+   * setter de pies
+   * @param numerito numero que se está en pies
+   */
+  setPies(numerito: number){
+    this.numero = numerito
+  }
+  /**
+   * getter de pies
+   * @returns pies
+   */
+  getPies() {
+    return this.numero
+  }
+  /**
+   * 
+   * @param numerito 
+   */
+  setPulgadas(numerito: number) {
+    this.numero = numerito * 12
+  }
+  getPulgadas() {
+    return this.numero / 12
+  }
+
+  setYardas(numerito: number) {
+    this.numero = numerito * 3
+  }
+  getYardas() {
+    return this.numero / 3
+  }
+
+  setMillas(numerito: number) {
+    this.numero = numerito * 5280
+  }
+  getMillas() {
+    return this.numero / 5280
+  }
 }
+
+export class MetricLength{
+  constructor(private numero: number = 0){}
+  setMetros(numerito: number){
+    this.numero = numerito
+  }
+  getMetros(){
+    return this.numero
+  }
+
+  setKilometros(numerito: number){
+    this.numero = numerito * 1000
+  }
+  getKilometros(){
+    return this.numero / 1000
+  }
+
+  setCentimetros(numerito: number){
+    this.numero = numerito / 100
+  }
+  getCentimetros(){
+    return this.numero * 100
+  }
+}
+//cliente trabaja en metrico y adapta pasa a imperial
+export class Adapter extends MetricLength{
+  constructor(private service: ImperialLength){
+    super();
+  }
+  getDatos(){
+    return this.service.getPies() / 3.281
+  }
+  setPies(numerito: number){
+    let variable = numerito / 3.281
+    this.service.setPies(numerito / 3.281)
+  }
+
+  getPies() {
+    return this.service.getPies() * 3.281
+  }
+
+  setPulgadas(numerito: number) {
+    this.service.setPulgadas(numerito / 39.37    )
+  }
+  getPulgadas() {
+    return this.service.getPulgadas() * 39.37
+  }
+
+  setYardas(numerito: number) {
+    this.service.setYardas(numerito / 1.094)
+  }
+  getYardas() {
+    return this.service.getYardas() * 1.094
+  }
+
+  setMillas(numerito: number) {
+    this.service.setMillas(numerito * 1609      )
+  }
+  getMillas() {
+    return this.service.getMillas() * 1609
+  }
+
+}
+
+const systemA = new MetricLength(281)
+const sistemB = new ImperialLength(85.6488)
+
+console.log(systemA.getMetros())
+
+const adaptador = new Adapter(sistemB)
+
+console.log(adaptador.getPies())
+
+
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,21 +1,88 @@
 import "mocha";
 import { expect } from "chai";
-import { add } from "../src/index";
+import { ImperialLength } from "../src/index";
+import { MetricLength } from "../src/index";
+import { Adapter } from "../src/index";
 
-describe("index test", () => {
-  it("add(1,4,5) == 10", () => {
-    expect(add(1, 4)).to.be.equal(5);
+let variable = new ImperialLength(10)
+
+describe("index imperial test", () => {
+  it("setpies", () => {
+    variable.setPies(20)
   });
-  it("add(1,4,5) == 10", () => {
-    expect(add(9, -2)).to.be.equal(7);
+  it("getpies", () => {
+    expect(variable.getPies()).to.be.equal(20);
   });
-  it("add(1,4,5) == 10", () => {
-    expect(add(1, 4)).to.not.be.equal(1);
+  it("getpies mal", () => {
+    expect(variable.getPies()).to.not.be.equal(10);
   });
-  it("add(1,4,5) == 10", () => {
-    expect(add(1, 4)).to.be.greaterThan(1);
+  it("setmillas", () => {
+    variable.setMillas(20)
   });
-  it("add(1,4,5) == 10", () => {
-    expect(add(1, 4)).to.be.lessThan(100000);
+  it("getmillas bien", () => {
+    expect(variable.getMillas()).to.be.equal(20);
+  });
+  it("getmillas mal", () => {
+    expect(variable.getMillas()).to.not.be.equal(10);
+  });
+  it("setyardas", () => {
+    variable.setYardas(20)
+  });
+  it("getyardas bien", () => {
+    expect(variable.getYardas()).to.be.equal(20);
+  });
+  it("getyardas mal", () => {
+    expect(variable.getYardas()).to.not.be.equal(10);
+  });
+  it("setpulgadas", () => {
+    variable.setPulgadas(20)
+  });
+  it("getpulgadas bien", () => {
+    expect(variable.getPulgadas()).to.be.equal(20);
+  });
+  it("getpulgadas mal", () => {
+    expect(variable.getPulgadas()).to.not.be.equal(10);
+  });
+
+});
+
+
+let metrico = new MetricLength(10)
+
+describe("index metrica test", () => {
+  it("setmetros", () => {
+    metrico.setMetros(20)
+  });
+  it("setmetros", () => {
+    expect(metrico.getMetros()).to.be.equal(20)
+  });
+  it("setmetros", () => {
+    metrico.setKilometros(20)
+  });
+  it("setmetros", () => {
+    expect(metrico.getKilometros()).to.be.equal(20)
+  });
+  it("setmetros", () => {
+    metrico.setCentimetros(20)
+  });
+  it("setmetros", () => {
+    expect(metrico.getCentimetros()).to.be.equal(20)
+  });
+});
+
+const systemA = new MetricLength(281)
+const sistemB = new ImperialLength(85.6488)
+
+const adaptador = new Adapter(sistemB)
+
+describe("index adaptador test", () => {
+  it("sistema metrico ", () => {
+    expect(systemA.getMetros()).to.be.equal(281)
+  });
+  it("sistema metrico ", () => {
+    systemA.setMetros(32)
+  });
+  it("sistema metrico ", () => {
+    expect(adaptador.getPies()).to.be.equal(281.0137128)
   });
 });
